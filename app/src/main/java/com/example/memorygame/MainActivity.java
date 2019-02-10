5e82ff79b766066330daeba9b72765e866c55a6d
Damian Stopa <stopa.damian997@gmail.com>
2019-02-11 00:56:17 +0100

    test activity + open

--- app/src/main/java/com/example/memorygame/MainActivity.java
+++ app/src/main/java/com/example/memorygame/MainActivity.java
@@ -1,31 +1,115 @@
 package com.example.memorygame;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_TAKE_PHOTO = 1;
+
+
+    private String mCurrentPhotoPath;
+
+    private ArrayList<String> photosPathsList;
+    private LinearLayout linearLayout;
+
+
+    private ArrayList<Bitmap> photosBitmaps;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //final ImageView imageView = new ImageView(this);
-        //imageView.setImageResource(R.drawable.zygzak);
-       // setContentView(imageView);
+
+        linearLayout = findViewById(R.id.linearLayout);
+
+        photosPathsList = new ArrayList<>();
+        photosBitmaps = new ArrayList<>();
 
     }
 
-    protected void onClickButtonTakePhoto(View view) {
 
+    private void takePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File...
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
     }
 
-    public void onClickStartGame(View v){
-
-        Intent intent = new Intent(this, GameActivity.class);
-        startActivity(intent);
-
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,
+                ".jpg",
+                storageDir
+        );
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
     }
+
+
+
+    protected void onClickButtonTakePhoto(View view)  {
+        takePictureIntent();
+    }
+
+    protected void onClickPlay(View view) {
+        //if (photosPathsList.size() == 4) {
+            //Intent intent = new Intent(this, GameActivity.class);
+            Intent intent = new Intent(this, TestActivity.class);
+            //intent.putStringArrayListExtra(EXTRA_MESSAGE, photosPathsList);
+            startActivity(intent);
+        //} else {
+           // alertNotEnoughPhotos();
+       // }
+    }
+
+    private void alertNotEnoughPhotos() {
+
+        new AlertDialog.Builder(MainActivity.this)
+                .setTitle("NOT ENOUGH PHOTOS")
+                .setMessage("You need 4 photos to play")
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                }).show();
+    }
+
 }
--- app/src/main/java/com/example/memorygame/PictureActivity.java
+++ app/src/main/java/com/example/memorygame/PictureActivity.java
@@ -2,15 +2,21 @@ package com.example.memorygame;
 
 
 import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
 
-public class PictureActivity {
+
+public  class PictureActivity extends BaseAdapter {
     private final Context context;
 
         public PictureActivity(Context context){
             this.context = context;
         }
 
-        public int getAmount(){
+        public int getCount(){
             return 8;
         }
 
@@ -18,7 +24,22 @@ public class PictureActivity {
             return null;
         }
 
-        public long getIdemId(int position){
-            return 0;
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView;
+        if (convertView == null) {
+            imageView = new ImageView(this.context);
+            imageView.setLayoutParams(new GridView.LayoutParams(250, 250));
+            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+        } else {
+            imageView = (ImageView) convertView;
         }
+        imageView.setImageResource(R.drawable.ic_photo_black_24dp);
+        return imageView;
+    }
 }
