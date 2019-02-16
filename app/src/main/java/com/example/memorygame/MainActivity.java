e0866303524afee92414b74e56a561395a19099e
Damian Stopa <stopa.damian997@gmail.com>
2019-02-17 00:23:14 +0100

    MemoryActivity - Bitmap

--- app/src/main/java/com/example/memorygame/MainActivity.java
+++ app/src/main/java/com/example/memorygame/MainActivity.java
@@ -2,36 +2,50 @@ package com.example.memorygame;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v4.content.FileProvider;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
     public class MainActivity extends AppCompatActivity {
 
-        static final int REQUEST_IMAGE_CAPTURE = 1;
-        static final int REQUEST_TAKE_PHOTO = 1;
+        private static final int REQUEST_IMAGE_CAPTURE = 1;
+        private static final int REQUEST_TAKE_PHOTO = 1;
 
-        ImageView mImageView;
-        String mCurrentPhotoPath;
+        public static final String EXTRA_MESSAGE = "com.example.memorygame.MESSAGE.PLAY";
+
+        private String mCurrentPhotoPath;
+        private ArrayList<String> photosPathsList;
+        private ArrayList<Bitmap> photosBitmaps;
 
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
+
+            photosPathsList = new ArrayList<>();
+            photosBitmaps = new ArrayList<>();
+
         }
 
+        @Override
+        protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+            if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+                photosPathsList.add(mCurrentPhotoPath);
+            }
+        }
 
         private void dispatchTakePictureIntent() {
             Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -42,7 +56,8 @@ import java.util.Date;
                 try {
                     photoFile = createImageFile();
                 } catch (IOException ex) {
-                    // Error occurred while creating the File
+                    Toast.makeText(MainActivity.this, " Error occurred while creating the File...", Toast.LENGTH_SHORT).show();
+                    ex.printStackTrace();
                 }
                 // Continue only if the File was successfully created
                 if (photoFile != null) {
@@ -55,71 +70,40 @@ import java.util.Date;
             }
         }
 
-        @Override
-        protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-            if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-                Bundle extras = data.getExtras();
-                Bitmap imageBitmap = (Bitmap) extras.get("data");
-
-                mImageView.setImageBitmap(imageBitmap);
-            }
-        }
-
         private File createImageFile() throws IOException {
             // Create an image file name
             String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
             String imageFileName = "JPEG_" + timeStamp + "_";
             File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-            File image = File.createTempFile(
+            File imageFile = File.createTempFile(
                     imageFileName,  /* prefix */
                     ".jpg",         /* suffix */
                     storageDir      /* directory */
             );
-
             // Save a file: path for use with ACTION_VIEW intents
-            mCurrentPhotoPath = image.getAbsolutePath();
-            return image;
+            mCurrentPhotoPath = imageFile.getAbsolutePath();
+            return imageFile;
         }
 
-        private void galleryAddPic() {
-            Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
-            File f = new File(mCurrentPhotoPath);
-            Uri contentUri = Uri.fromFile(f);
-            mediaScanIntent.setData(contentUri);
-            this.sendBroadcast(mediaScanIntent);
-        }
-
-        private void setPic() {
-            // Get the dimensions of the View
-            int targetW = mImageView.getWidth();
-            int targetH = mImageView.getHeight();
-
-            // Get the dimensions of the bitmap
-            BitmapFactory.Options bmOptions = new BitmapFactory.Options();
-            bmOptions.inJustDecodeBounds = true;
-            BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
-            int photoW = bmOptions.outWidth;
-            int photoH = bmOptions.outHeight;
-
-            // Determine how much to scale down the image
-            int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
-
-            // Decode the image file into a Bitmap sized to fill the View
-            bmOptions.inJustDecodeBounds = false;
-            bmOptions.inSampleSize = scaleFactor;
-            bmOptions.inPurgeable = true;
-
-            Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
-            mImageView.setImageBitmap(bitmap);
-        }
-
-        public void onClickButtonTakePhoto(View view) {
+        protected void onClickButtonTakePhoto(View view) {
             dispatchTakePictureIntent();
         }
 
-        public void onClickPlay(View view) {
-
-            Intent intent = new Intent(this,MemoryActivity.class);
-            startActivity(intent);
+        protected void onClickPlay(View view) {
+            if (photosPathsList.size() == 4) {
+                Intent intent = new Intent(this, MemoryActivity.class);
+                intent.putStringArrayListExtra(EXTRA_MESSAGE, photosPathsList);
+                startActivity(intent);
+            } else {
+                alert();
+            }
         }
+
+        private void alert() {
+            new AlertDialog.Builder(MainActivity.this)
+                    .setTitle("INCORRECT AMOUNT OF PHOTOS")
+                    .setMessage("You need 4 photos to play.")
+                    .show();
+        }
+
     }
--- app/src/main/java/com/example/memorygame/MemoryActivity.java
+++ app/src/main/java/com/example/memorygame/MemoryActivity.java
@@ -1,25 +1,69 @@
 package com.example.memorygame;
 
-import android.os.Build;
-import android.support.annotation.RequiresApi;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 
 public class MemoryActivity extends AppCompatActivity {
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_memory);
+    private static final int PHOTO_WIDTH = 300;
+    private static final int PHOTO_HEIGHT = 300;
 
+    private ArrayList<Bitmap> photoBitmaps;
 
+    private int[] position = {0, 1, 2, 3, 0, 1, 2, 3};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_memory);
+
+        Intent intent = getIntent();
+        ArrayList<String> photoFilePaths = intent.getStringArrayListExtra(MainActivity.EXTRA_MESSAGE);
+        photoBitmaps = listToBitmap(photoFilePaths);
+        mixArray(position);
+    }
+
+    private ArrayList<Bitmap> listToBitmap(ArrayList<String> photoFilePaths) {
+        ArrayList<Bitmap> bitmaps = new ArrayList<>();
+        for (String photo : photoFilePaths) {
+            bitmaps.add(photoToBitmap(photo));
         }
+        return bitmaps;
+    }
 
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-    public void onClickComeBack(View view) {
-            this.finishAndRemoveTask();
+    private Bitmap photoToBitmap(String mCurrentPhotoPath) {
+
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+        int photoWidth = bmOptions.outWidth;
+        int photoHeight = bmOptions.outHeight;
+
+        int scaleFactor = Math.min(photoWidth / MemoryActivity.PHOTO_WIDTH, photoHeight / MemoryActivity.PHOTO_HEIGHT);
+
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        bmOptions.inPurgeable = true;
+
+        return BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+    }
+
+    private static void mixArray(int[] array) {
+        int index, temp;
+        Random random = new Random();
+        for (int i = array.length - 1; i > 0; i--) {
+            index = random.nextInt(i + 1);
+            temp = array[index];
+            array[index] = array[i];
+            array[i] = temp;
+        }
     }
 }
 
