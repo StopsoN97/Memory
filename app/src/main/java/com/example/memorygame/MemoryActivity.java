05eb9eb0c0eb79ab8aacaea978ddc0b3ea513965
Damian Stopa <stopa.damian997@gmail.com>
2019-02-17 22:31:29 +0100

    comparing photos

--- app/src/main/java/com/example/memorygame/MemoryActivity.java
+++ app/src/main/java/com/example/memorygame/MemoryActivity.java
@@ -3,8 +3,14 @@ package com.example.memorygame;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Random;
@@ -12,12 +18,14 @@ import java.util.Random;
 
 public class MemoryActivity extends AppCompatActivity {
 
-    private static final int PHOTO_WIDTH = 300;
-    private static final int PHOTO_HEIGHT = 300;
+    private static final int PHOTO_WIDTH = 150;
+    private static final int PHOTO_HEIGHT = 150;
 
     private ArrayList<Bitmap> photoBitmaps;
-
+    private ImageView currentView = null;
+    private int countDoubles = 0;
     private int[] position = {0, 1, 2, 3, 0, 1, 2, 3};
+    private int currentPosition = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +36,11 @@ public class MemoryActivity extends AppCompatActivity {
         ArrayList<String> photoFilePaths = intent.getStringArrayListExtra(MainActivity.EXTRA_MESSAGE);
         photoBitmaps = listToBitmap(photoFilePaths);
         mixArray(position);
+
+        PictureAdapter picAdapter = new PictureAdapter(this);
+        GridView gridView = findViewById(R.id.gridviewAB);
+        gridView.setAdapter(picAdapter);
+        gridView.setOnItemClickListener(gridViewOnItemClickListener);
     }
 
     private ArrayList<Bitmap> listToBitmap(ArrayList<String> photoFilePaths) {
@@ -37,6 +50,48 @@ public class MemoryActivity extends AppCompatActivity {
         }
         return bitmaps;
     }
+
+    private AdapterView.OnItemClickListener gridViewOnItemClickListener = new AdapterView.OnItemClickListener() {
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            if (currentPosition < 0) {
+                currentPosition = position;
+                currentView = (ImageView) view;
+                Bitmap bitmap = photoBitmaps.get(MemoryActivity.this.position[position]);
+                ((ImageView) view).setImageBitmap(bitmap);
+
+            } else {
+                if (currentPosition == position) {
+                    ((ImageView) view).setImageResource(R.drawable.logo);
+                } else if (MemoryActivity.this.position[currentPosition] != MemoryActivity.this.position[position]) {
+                    Toast.makeText(MemoryActivity.this, "Not Match!", Toast.LENGTH_LONG).show();
+
+                    Bitmap bitmap = photoBitmaps.get(MemoryActivity.this.position[position]);
+                    ((ImageView) view).setImageBitmap(bitmap);
+                    final View viewHandler = view;
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            ((ImageView) viewHandler).setImageResource(R.drawable.logo);
+                            currentView.setImageResource(R.drawable.logo);
+                        }
+                    }, 800); // Millisecond 1000 = 1 sec
+
+                } else {
+                    Bitmap bitmap = photoBitmaps.get(MemoryActivity.this.position[position]);
+                    ((ImageView) view).setImageBitmap(bitmap);
+                    Toast.makeText(MemoryActivity.this, "Match!", Toast.LENGTH_LONG).show();
+                    countDoubles++;
+                    if (countDoubles == 4) {
+                        Toast.makeText(MemoryActivity.this, "You Win!", Toast.LENGTH_LONG).show();
+                    }
+                }
+                currentPosition = -1;
+            }
+        }
+
+    };
 
     private Bitmap photoToBitmap(String mCurrentPhotoPath) {
 
---
+++ app/src/main/java/com/example/memorygame/PictureAdapter.java
@@ -0,0 +1,47 @@
+package com.example.memorygame;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+public class PictureAdapter extends BaseAdapter{
+
+    private final Context context;
+
+    public PictureAdapter(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return 8;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView;
+        if (convertView == null) {
+            imageView = new ImageView(this.context);
+            imageView.setLayoutParams(new GridView.LayoutParams(150, 150));
+            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+        } else {
+            imageView = (ImageView) convertView;
+        }
+        imageView.setImageResource(R.drawable.logo);
+        return imageView;
+    }
+}
+
